e4d0bff1b7aed7311c2c0c80a4c4314720019845
jamie-codez <cruiseomondi90@gmail.com>
2024-03-06 05:18:46 +0300

    Repo updated

--- src/books/books.repo.provider.ts
+++ src/books/books.repo.provider.ts
@@ -1,11 +1,20 @@
-import { BOOKS_REPOSITORY, DATABASE_CONNECTION } from '../constants';
+import {
+  BOOKS_REPOSITORY,
+  DATABASE_CONNECTION,
+  LENDS_REPOSITORY,
+} from '../constants';
 import { DataSource } from 'typeorm';
-import { Book } from './entities/book.entity';
+import { Book, Lend } from './entities/book.entity';
 
 export const booksRepoProvider = [
   {
     provide: BOOKS_REPOSITORY,
     useFactory: (connection: DataSource) => connection.getRepository(Book),
+    inject: [DATABASE_CONNECTION],
+  },
+  {
+    provide: LENDS_REPOSITORY,
+    useFactory: (connection: DataSource) => connection.getRepository(Lend),
     inject: [DATABASE_CONNECTION],
   },
 ];
