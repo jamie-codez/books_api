c57187d9a68189b1f23f991c3ae200871ec0df9f
jamie-codez <cruiseomondi90@gmail.com>
2024-03-06 05:34:47 +0300

    Seeding

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,6 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as process from 'process';
 import { ValidateInputPipe } from './pipes/validate.pipes';
-import { Client } from 'pg';
-import fs from 'fs';
-import path from 'path';
-import { config } from 'dotenv';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -22,25 +18,5 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidateInputPipe());
   await app.listen(process.env.APP_PORT || 3000);
 }
+
 bootstrap();
-
-config();
-async function seed() {
-  const client = new Client({
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  });
-
-  await client.connect();
-
-  const sql = fs.readFileSync(path.join(__dirname, 'init.sql')).toString();
-
-  await client.query(sql);
-
-  await client.end();
-}
-
-seed().catch((err) => console.error(err));
